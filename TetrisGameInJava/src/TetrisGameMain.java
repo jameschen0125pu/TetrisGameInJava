939b256742472f10a0672b7847ac3f2b76f497da
James Chen <jameschen0125@pu.edu.tw>
2026-06-05 21:48:43 +0800

    Test Git Commit Only

--- TetrisGameInJava/src/TetrisGameMain.java
+++ TetrisGameInJava/src/TetrisGameMain.java
@@ -6,6 +6,7 @@ public class TetrisGameMain {
 	public static void main(String[] args) {
 		// 使用 SwingUtilities.invokeLater 將 UI 啟動放到 EDT（Event Dispatch Thread）中，確保 Swing 的執行緒安全
 		SwingUtilities.invokeLater(() -> {
+			// Test Git Only
 			new TetrisGameUI(); // 建立並顯示主要遊戲視窗
 		});
 	}
